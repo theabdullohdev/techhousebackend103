304e047ebd7141f74f547e35f0a055e33bacdfa1
abdulloxtoxtasinov10-cloud <abdulloxtoxtasinov10@gmail.com>
2026-04-20 10:48:59 +0500

    .

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,7 +1,34 @@
-export function getProducts() {}
-export function getProduct() {}
-export function createProduct() {}
-export function deleteProduct() {}
-export function updateProduct() {}
+import {products} from "../data/db.js"
 
+export function getProducts(req, res) {
+    const id = req.params.id;
+    const product = products.find(p => p.id == id);
+    if (!product) {
+        return res.status(404).json({ success: false, message: "Product not found" });
+    }
+    return res.status(200).json({ product });
+}
+
+export function getProduct(req, res) {
+
+    returnres.status(200).json({ products });
+}
+export function createProduct(req, res) {}
+
+
+export function deleteProduct(req, res) {
+    const id = req.params.id;
+
+    const {name, amount} = req.body;
+
+    const product = products.find(p => p.id == id);
+    if (!product) {
+        return res.status(404).json({ success: false, message: "Product not found" });
+    }
+    return res.status(200).json({ product });
+
+    if
+
+}
+export function updateProduct(req, res) {}
 
--- backend/src/data/db.js
+++ backend/src/data/db.js
@@ -1,2 +1,15 @@
-const users = [];
-export default users;
+export const users = [];
+export const products = [
+    {
+    id: 1,
+    name: "Milk",
+    amount: "2.5L"
+}
+
+{
+    id: 2,
+    name: "Fanta",
+    amount: "2.5L"
+}
+        
+];
